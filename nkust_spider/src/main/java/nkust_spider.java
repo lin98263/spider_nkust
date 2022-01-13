b851dd9887fc4f7d638885fc9af6269e08659748
lin98263 <93572449+lin98263@users.noreply.github.com>
2022-01-13 11:09:10 +0800

    Add files via upload

---
+++ nkust_spider/src/main/java/nkust_spider.java
@@ -0,0 +1,10 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Scanner;
+
+public class nkust_spider {
+
+}
