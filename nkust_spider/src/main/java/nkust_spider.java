6b652fe2e38d6671923182dcc534fd6f616842a5
Leo0811time <0811alanleo@gmail.com>
2022-01-13 11:16:47 +0800

    輸入輸出

--- nkust_spider/src/main/java/nkust_spider.java
+++ nkust_spider/src/main/java/nkust_spider.java
@@ -6,5 +6,13 @@ import org.jsoup.select.Elements;
 import java.util.Scanner;
 
 public class nkust_spider {
+    public static void main(String[] args) {
 
+        String champion, lane;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("輸入欲查詢英雄:");
+        champion = scanner.next();
+        System.out.print("輸入欲查詢路線(top/jungle/mid/adc/support):");
+        lane = scanner.next();
 }
